f162f149c8abf83f3bc02785a61626441dc8b1b6
briansai <brianwsai@gmail.com>
2018-06-30 16:19:02 -0700

    config for redis

--- SDC/CRUD/controller.js
+++ SDC/CRUD/controller.js
@@ -5,24 +5,13 @@ const bodyParser = require('body-parser');
 // const cluster = require('cluster');
 // const numCPUs = require('os').cpus();
 const pdb = require('./psqlModel.js');
+const client = require('./redisConfig.js');
 
 const app = express();
 const PORT = 3000;
 // app.use(express.static(path.join(__dirname, '../../public/')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const redis = require('redis');
-
-const client = redis.createClient('6379', '127.0.0.1');
-client.auth('A1aAl3UDjW9WUN5oLKkq2Ajg3Tn/ee1z6wfd+EuCYFfAkKgGw1JYeAGOKZNu2KeO/9VN5fD9r6A5DEzE4')
-
-client.on('error', (err) => {
-  console.log(err);
-});
-
-client.config('SET', 'maxmemory', '500mb');
-client.config('SET', 'maxmemory-policy', 'allkeys-lfu');
 
 const cache = (req, res, next) => {
   const param = req.params.id;
@@ -39,7 +28,6 @@ const cache = (req, res, next) => {
     }
   });
 };
-
 
 app.get('/getReviews/:id', cache, (req, res) => {
   const param = req.params.id;
